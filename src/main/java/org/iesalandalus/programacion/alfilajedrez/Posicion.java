4122bfbb0734f90e4afb0c589fbb0f6197b580d7
lemon <lemon@lemon-Lenovo-B50-70>
2020-11-11 13:52:55 +0100

    7.Creo constructor con parámetros

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -5,25 +5,39 @@ public class Posicion {
 	// Declaro variables
 	private int fila;
 	private char columna;
-	
-	//METODOS GET Y SET
+
+	// CONTRUCTORES
+	public Posicion(int fila, char columna) {
+		//COMPROBAR ESTA EXCEPCION EN UN FUTURO
+		try {
+			this.fila = fila;
+			this.columna = columna;
+		} catch (IllegalArgumentException e) {
+			System.out.println("Los valores introducidos no son correctos.");
+		}
+	}
+
+	// METODOS GET Y SET
 	public int getFila() {
 		return fila;
 	}
+
 	private void setFila(int fila) {
-		if (fila<1 || fila >8)
-			throw new IllegalArgumentException("El valor de fila no es correcto");
+		if (fila < 1 || fila > 8)
+			throw new IllegalArgumentException("El valor de fila no es correcto.");
 		else
-		this.fila = fila;
+			this.fila = fila;
 	}
+
 	public char getColumna() {
 		return columna;
 	}
+
 	private void setColumna(char columna) {
-		if (columna<'a' || columna>'h')
-			throw new IllegalArgumentException("El valor de columna no es correcto");
+		if (columna < 'a' || columna > 'h')
+			throw new IllegalArgumentException("El valor de columna no es correcto.");
 		else
-		this.columna = columna;
+			this.columna = columna;
 	}
-	
+
 }
