f409d0d54f6d40eaacf30967e5b45af49550dd99
lemon <lemon@lemon-Lenovo-B50-70>
2020-11-11 13:03:25 +0100

    6. Creo métodos get y set(Posicion)

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -5,32 +5,25 @@ public class Posicion {
 	// Declaro variables
 	private int fila;
 	private char columna;
-
-	// Creo los contructores
-	public Posicion(int fila, char columna) {
-
-	}
-
-
-	// Genero métodos get y set
+	
+	//METODOS GET Y SET
 	public int getFila() {
 		return fila;
 	}
-
 	private void setFila(int fila) {
-		if (fila >= 1 && fila <= 8)
-			this.fila = fila;
-		// else PONER EXCEPCION AQUI
+		if (fila<1 || fila >8)
+			throw new IllegalArgumentException("El valor de fila no es correcto");
+		else
+		this.fila = fila;
 	}
-
 	public char getColumna() {
 		return columna;
 	}
-
 	private void setColumna(char columna) {
-		if (columna >= 'a' && columna <= 'h')
-			this.columna = columna;
-		// else PONER EXCEPCION AQUI
+		if (columna<'a' || columna>'h')
+			throw new IllegalArgumentException("El valor de columna no es correcto");
+		else
+		this.columna = columna;
 	}
-
+	
 }
