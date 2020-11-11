babde5978a9c4500b0f71f56a7cf2c9aa9024d04
lemon <lemon@lemon-Lenovo-B50-70>
2020-11-11 11:54:32 +0100

    3.Creo enumerado Color

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -1,9 +1,36 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
 public class Posicion {
-	
-	//Declaro variables
+
+	// Declaro variables
 	private int fila;
 	private char columna;
-	
+
+	// Creo los contructores
+	public Posicion(int fila, char columna) {
+
+	}
+
+
+	// Genero métodos get y set
+	public int getFila() {
+		return fila;
+	}
+
+	private void setFila(int fila) {
+		if (fila >= 1 && fila <= 8)
+			this.fila = fila;
+		// else PONER EXCEPCION AQUI
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setColumna(char columna) {
+		if (columna >= 'a' && columna <= 'h')
+			this.columna = columna;
+		// else PONER EXCEPCION AQUI
+	}
+
 }
