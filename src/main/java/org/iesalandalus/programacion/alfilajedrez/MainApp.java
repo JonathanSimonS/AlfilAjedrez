c70d835d08cc1ead8608a6f0761fdc156a8d2699
lemon <lemon@lemon-Lenovo-B50-70>
2020-11-23 18:54:18 +0100

    19.2 void mostrarMenu()

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -15,7 +15,7 @@ public class MainApp {
 		 */
 		int opcion;
 		mostrarAlfil();
-		//mostrarMenu();
+		mostrarMenu();
 
 	}
 
@@ -24,7 +24,19 @@ public class MainApp {
 		System.out.println(Alfil.ALFIL);
 	}
 
-	
+	private static void mostrarMenu() {
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("Menú con las diferentes opciones ");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("");
+		System.out.println("1.Crear un alfil por defecto. ");
+		System.out.println("2.Crear un alfil de un color. ");
+		System.out.println("3.Crear un alfil de un color en una columna inicial dada ('c' o 'f'). ");
+		System.out.println("4.Mover el alfil.");
+		System.out.println("");
+		System.out.println("0. Salir");
+		System.out.println("");
+	}
 	
 	
 	
